0c6ecb88b5803bcdaa5d0d4eb31a2fb0280c2f1f
HALKBANK <adilgorkem.celebi2@halkbank.com.tr>
2023-11-28 16:04:06 +0300

    main screen changed

--- main_screen.py
+++ main_screen.py
@@ -1,18 +1,19 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, simpledialog
 
 from full_screen import full_screen
 
+
 class MainScreen(tk.Toplevel):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__()
         self.parent = parent
         self.win = tk.Tk()
         self.win.title('Main Screen')
         self.win.geometry(full_screen(self.win))
         self.win.resizable(True, True)
-        self.labels = []
-        self.list_box = tk.Listbox(self.win, height=self.win.winfo_screenheight(), selectmode='SINGLE')
+        self.book_list = []  # Her bir kitap öğesini içeren liste
+        self.list_box = tk.Listbox(self.win, height=50,width=50, selectmode='SINGLE')
         self.bottom_frame = tk.Frame(self.win)
         self.book_id_label = ttk.Label(self.bottom_frame, text=f"Book id: ")
         self.book_name_label = ttk.Label(self.bottom_frame, text=f"Book name: ")
@@ -20,18 +21,20 @@ class MainScreen(tk.Toplevel):
         self.list_box.bind("<ButtonRelease-1>", self.onItemSelect)
         self.addButton = None
         self.deleteButton = None
-        if(parent.usernameEntry.get() == "user"):
+
+        if parent.usernameEntry.get() == "user":
             print("User logged")
-        elif():
-            print("Admin logged")      
+        elif parent.usernameEntry.get() == "admin":
+            print("Admin logged")
+
         self.create_widgets()
 
     def listMaker(self):
         for i in range(50):
-            self.label_text = f"Label {i}"
-
-            self.labels.append(self.label_text)
-            self.list_box.insert(i, self.labels[i])
+            book_info = {'id': i + 1, 'book_name': f"Book Label", 'author_name': f"Author"}
+            self.book_list.append(book_info)
+            display_text = f"{book_info['book_name']} - {book_info['author_name']} - ID: {book_info['id']}"
+            self.list_box.insert(tk.END, display_text)
 
     def create_widgets(self):
         self.bottom_frame.pack(side=tk.RIGHT, anchor="ne", pady=(20, 0), padx=(0, 20))
@@ -39,14 +42,29 @@ class MainScreen(tk.Toplevel):
         self.book_name_label.pack(padx=(0, 500), pady=(20, 0), anchor="nw")
         self.author_name_label.pack(padx=(0, 500), pady=(20, 0), anchor="nw")
         self.list_box.pack(pady=(20, 0), padx=(20, 0), anchor="nw")
-        self.listMaker() 
+
+        # Add Book button
+        self.add_book_button = tk.Button(self.bottom_frame, text="Add Book", command=self.add_book)
+        self.add_book_button.pack(side=tk.TOP)
+
+        self.listMaker()
 
     def onItemSelect(self, event):
         self.selected_item_index = self.list_box.curselection()
 
-        if(self.selected_item_index):
-            self.selected_item = self.list_box.get(self.selected_item_index[0])
+        if self.selected_item_index:
+            selected_item_info = self.book_list[self.selected_item_index[0]]
 
-        self.book_name_label.configure(text=f"Book name: {self.selected_item}")
-        self.book_id_label.configure(text=f"Book id: {self.selected_item_index[0]}")
-        #TO-DO author label    
+            self.book_name_label.configure(text=f"Book name: {selected_item_info['book_name']}")
+            self.book_id_label.configure(text=f"Book ID: {selected_item_info['id']}")
+            self.author_name_label.configure(text=f"Author name: {selected_item_info['author_name']}")
+
+    def add_book(self):
+        book_name = simpledialog.askstring("Add Book", "Enter the book name:")
+        author_name = simpledialog.askstring("Add Book", "Enter the author name:")
+
+        if book_name and author_name is not None:
+            book_info = {'book_name': book_name, 'author_name': author_name}
+            self.book_list.append(book_info)
+            display_text = f"Book: {book_info['book_name']} - Author: {book_info['author_name']} - ID: {len(self.book_list)}"
+            self.list_box.insert(tk.END, display_text)
